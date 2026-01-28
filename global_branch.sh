95356a74b960f5a105587ff1694085b2accb8dd3
wbullaughey <wlb122@verizon.net>
2026-01-28 13:04:36 -0500

    update global_branch.sh

--- global_branch.sh
+++ global_branch.sh
@@ -10,41 +10,87 @@ function branch(){
    MODULE=$1
    echo branch module $MODULE
    if [[ -d "$MODULE" ]]; then
-      pushd $MODULE >/dev/null 2>&1
-      if [[ $? -eq 0 ]]; then
-         echo "checkout $MODULE" with "$BRANCH"
-         git checkout -b "$BRANCH"
-         if [[ $? -eq 0 ]]; then
-            echo $MODULE checkout
-            git push --set-upstream origin $BRANCH
-#           git push
-            if [[ $? -eq 0 ]]; then
-               echo $MODULE pushed
-            else
-               echo push $MODULE failed
-               exit
-            fi
-         else
-            echo branch $MODULE failed
-#            exit
-         fi
-         popd >/dev/null 2>&1
-      else
-          echo "pushd failed"
-          exit
-      fi
+      echo $MODULE exists
    else
-       echo "Directory $MODULE does not exist"
+      echo $MODULE does not exists
+      exit
+   fi
+
+   pushd $MODULE >/dev/null 2>&1
+   echo pushd to `pwd`
+   if [[ $? -eq 0 ]]; then
+      echo pushd to `cmd`
+   else
+      echo pushd to $MODULE failed
+      exit
+   fi
+
+   echo check in any changes for "$MODULE"
+   git add .
+   git commit -m "branch $BRANCH"
+   if [[ $? -eq 0 ]]; then
+      echo module $MODULE checked in
+   else
+      echo module $MODULE check in failed
+      exit
+   fi
+
+   echo push module $MODULE
+   git push
+   if [[ $? -eq 0 ]]; then
+      echo module $MODULE pushd
+   else
+      echo module $MODULE push failed
+      exit
+   fi
+
+   echo "switch $MODULE" with "$BRANCH"
+   git switch -c "$BRANCH"
+   if [[ $? -eq 0 ]]; then
+      echo module "$MODULE" switched to "$BRANCH"
+   else
+      echo switch to $MODULE failed
+      exit
+   fi
+
+   popd >/dev/null 2>&1
+   echo poped to `pwd`
+}
+
+function push (){
+   MODULE=$1
+   pushd $MODULE >/dev/null 2>&1
+   if [[ $? -eq 0 ]]; then
+      echo pushd to `pwd`
+      popd >/dev/null 2>&1
+      echo poped to `pwd`
+   else
+       echo "pushd failed"
        exit
    fi
+
+   git push --set-upstream origin $BRANCH
+   if [[ $? -eq 0 ]]; then
+      echo $MODULE pushed
+   else
+      echo push $MODULE failed
+      exit
+   fi
+   popd
+   echo poped to `pwd`
 }
 
 branch "ada_lib"
-branch "ada_lib/ada_lib_test_lib"
-branch "ada_lib/ada_lib_tests"
 branch "applications"
 branch "aunit"
 branch "gnoga_lib"
 branch "vendor/github.com/gnoga"
 branch "."
+
+push "ada_lib"
+push "applications"
+push "aunit"
+push "gnoga_lib"
+push "vendor/github.com/gnoga"
+push "."
 
